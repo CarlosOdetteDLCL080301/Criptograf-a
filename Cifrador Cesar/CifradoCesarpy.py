cd5da5ccdd5c8354f69f7310dcb4f41fcc86b672
Carlos Odette DLCL <carlosodettedlcl@gmail.com>
2024-02-27 23:29:26 -0600

    Se actualiza el cifrado, ahora se eliminan las letras repetidasen la llave

--- Cifrador Cesar/CifradoCesarpy.py
+++ Cifrador Cesar/CifradoCesarpy.py
@@ -1,36 +1,60 @@
+#Función para limpiar la llave, eliminando las palabras que se repiten
 def limpiarCadena(llave):
-    for letra in llave:
-        l = llave.find(letra)
-        mantener = llave[:l+1]
-        modificar = llave[l+1:]
+    #Recorremos la llave letra por letra
+    for letra in llave: 
+        #Buscamos la letra en la llave
+        l = llave.find(letra) 
+        #Guardamos la parte de la llave que no se va a modificar considerando a partir del punto de referencia para no modificar la llave
+        mantener = llave[:l+1] 
+        # Guardamos la parte de la llave que se va a modificar considerando a partir del punto de referencia
+        modificar = llave[l+1:]  
+        #Eliminamos la letra que se repite
         modificar= modificar.replace(letra, "")
+        #Unimos las dos partes de la llave
         llave = mantener + modificar
+    #Regresamos la llave limpia
     return llave
 
+# Creamos una función para cifrar un texto con el cifrado César
 def cifrado_cesar(texto, desplazamiento):
     resultado = ""
     for letra in texto:
-        if letra.isalpha(): # Comprobamos que sea una letra
-            if letra.isupper(): # Comprobamos si es mayúscula
-                codigo = ord(letra) - ord('A') # Convertimos la letra a un número entre 0 y 25
-                codigo = (codigo + desplazamiento) % 26 # Aplicamos el desplazamiento considerando que si se pasa de 25 debe volver a empezar
-                resultado += chr(codigo + ord('A')) # Convertimos el número resultante a letra y lo añadimos al texto cifrado
+        # Comprobamos que sea una letra
+        if letra.isalpha(): 
+            # Comprobamos si es mayúscula
+            if letra.isupper(): 
+                # Convertimos la letra a un número entre 0 y 25
+                codigo = ord(letra) - ord('A') 
+                # Aplicamos el desplazamiento considerando que si se pasa de 25 debe volver a empezar
+                codigo = (codigo + desplazamiento) % 26
+                # Convertimos el número resultante a letra y lo añadimos al texto cifrado 
+                resultado += chr(codigo + ord('A')) 
             else:
-                codigo = ord(letra) - ord('a')# Comprobamos que sea una letra
-                codigo = (codigo + desplazamiento) % 26# Aplicamos el desplazamiento considerando que si se pasa de 25 debe volver a empezar
-                resultado += chr(codigo + ord('a'))# Convertimos el número resultante A letra y lo añadimos al texto cifrado
+                # Comprobamos que sea una letra
+                codigo = ord(letra) - ord('a')
+                # Aplicamos el desplazamiento considerando que si se pasa de 25 debe volver a empezar
+                codigo = (codigo + desplazamiento) % 26
+                # Convertimos el número resultante A letra y lo añadimos al texto cifrado
+                resultado += chr(codigo + ord('a'))
         else:
-            resultado += letra # Si no es una letra lo añadimos tal cual
+            # Si no es una letra lo añadimos tal cual
+            resultado += letra 
+    # Retornamos el texto cifrado
     return resultado
 
-#Se recibe la cadena
-texto_original = "aA bB cC"
+#Se recibe la cadena que se desea cifrar
+texto_original = "Por mi raza hablara el espiritu"
+# Se recibe una llave
 llave = "DELACRUZLOPEZ"
+# Si la llave, tiene letras repetidas, se eliminan para reducir las posibilidades de cifrarlo
 texto_limpio = limpiarCadena(llave)
-print(texto_limpio)
-for letraLlave in llave:
+# Se recorre la llave para cifrar el texto con cada letra de la llave
+for letraLlave in texto_limpio:
+    # Se verifica que la letra sea una letra
     if letraLlave.isalpha():
+        # Se obtiene el desplazamiento de la letra
         desplazamiento = ord(letraLlave) - ord('A') if letraLlave.isupper() else ord(letraLlave) - ord('a')
-        print(desplazamiento)
+        # Se cifra el texto con el desplazamiento obtenido
         texto_cifrado = cifrado_cesar(texto_original, desplazamiento)
-        print(f"El texto {texto_original} cifrado con la llave {letraLlave} es: {texto_cifrado}")
+        # Se imprime el texto cifrado
+        print(f"El texto ''{texto_original}'' cifrado con la llave {letraLlave} es: '{texto_cifrado}'")
